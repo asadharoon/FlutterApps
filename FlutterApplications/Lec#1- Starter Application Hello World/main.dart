46ba5e17f598353846a80d4f49ff11feea2596e4
Asad Haroon <45485946+asadharoon@users.noreply.github.com>
2020-01-20 20:33:54 +0500

    Tutorials

---
+++ FlutterApplications/Lec#1- Starter Application Hello World/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// starting point function of the flutter app
+void main() => runApp(
+      MaterialApp(
+        home: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
